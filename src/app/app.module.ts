067786938ef4575d33227868cda3c788f0932237
zaleck-mov <alexisgunitt@gmail.com>
2021-03-28 15:42:15 -0600

    estructuta de archivos y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafic1Component } from './pages/grafic1/grafic1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    NopagefoundComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ProgressComponent,
+    Grafic1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
